b30d644184055c9993e089bbe6b2429b0eb6a34a
rutvikshiyani_crest <rutvik.shiyani@crestdatasys.com>
2021-12-31 15:53:09 +0530

    updated test files

--- testacc/data_source_aci_fabrichifpol_test.go
+++ testacc/data_source_aci_fabrichifpol_test.go
@@ -29,11 +29,11 @@ func TestAccAciFabricIFPolDataSource_Basic(t *testing.T) {
 			{
 				Config: CreateAccFabricIFPolConfigDataSource(rName),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, "name", resourceName, "name"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "description", resourceName, "description"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "annotation", resourceName, "annotation"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "name_alias", resourceName, "name_alias"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "auto_neg", resourceName, "auto_neg"),
-					resource.TestCheckResourceAttrPair(dataSourceName, "dfe_delay_ms", resourceName, "dfe_delay_ms"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "fec_mode", resourceName, "fec_mode"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "link_debounce", resourceName, "link_debounce"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "speed", resourceName, "speed"),
@@ -42,6 +42,10 @@ func TestAccAciFabricIFPolDataSource_Basic(t *testing.T) {
 			{
 				Config:      CreateAccFabricIFPolDataSourceUpdate(rName, randomParameter, randomValue),
 				ExpectError: regexp.MustCompile(`An argument named (.)+ is not expected here.`),
+			},
+			{
+				Config:      CreateAccFabricIFPolDataSourceUpdatedName(rName, randomParameter, randomValue),
+				ExpectError: regexp.MustCompile(`Object may not exists`),
 			},
 
 			{
@@ -66,6 +70,26 @@ func CreateAccFabricIFPolConfigDataSource(rName string) string {
 	data "aci_fabric_if_pol" "test" {
 	
 		name  = aci_fabric_if_pol.test.name
+		depends_on = [
+			aci_fabric_if_pol.test
+		]
+	}
+	`, rName)
+	return resource
+}
+
+func CreateAccFabricIFPolConfigDataSourceUpdatedName(rName string) string {
+	fmt.Println("=== STEP  testing fabric_if_pol creation with Invalid Name")
+	resource := fmt.Sprintf(`
+	
+	resource "aci_fabric_if_pol" "test" {
+	
+		name  = "%s"
+	}
+
+	data "aci_fabric_if_pol" "test" {
+	
+		name  = "${aci_fabric_if_pol.test.name}_invaid"
 		depends_on = [
 			aci_fabric_if_pol.test
 		]
